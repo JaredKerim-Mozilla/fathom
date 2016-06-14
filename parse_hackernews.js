e307362e92f9df1ff41aecb4c5d777c217192a47
Jared Kerim <jared.l.kerim@gmail.com>
2016-06-14 17:06:29 +0000

    Separate metadata into function

--- parse_hackernews.js
+++ parse_hackernews.js
@@ -16,6 +16,33 @@ const titleRules = ruleset(
 //const titleNode = kb.max('title');
 
 
+const metadataRules = {
+  title: titleRules,
+}
+
+
+function getUrlMetadata(url, resolve) {
+  console.log('entering promise');
+  jsdom.env({
+    url: url,
+    done: function (err, window) {
+      console.log('processing');
+      if (!window) {
+        return
+      }
+      const articleDocument = window.document;
+      const titleKB = titleRules.score(articleDocument);
+      const titleNode = titleKB.max('title');
+      if (titleNode) {
+        console.log(titleNode.flavors.get('title'));
+        resolve(titleNode.flavors.get('title'));
+      }
+      console.log('---------')
+    }
+  });
+}
+
+
 jsdom.env({
   url: 'http://news.ycombinator.com/',
   done: function (err, window) {
@@ -25,22 +52,8 @@ jsdom.env({
     console.log('HN Links');
     const links = Array.from(document.querySelectorAll('a.storylink')).map((link) => {
       const articleUrl = link.getAttribute('href');
-
-      jsdom.env({
-        url: articleUrl,
-        done: function (err, window) {
-          if (!window) {
-            return
-          }
-          console.log(articleUrl);
-          const articleDocument = window.document;
-          const titleKB = titleRules.score(articleDocument);
-          const titleNode = titleKB.max('title');
-          if (titleNode) {
-            console.log(titleNode.flavors.get('title'));
-          }
-          console.log('---------')
-        }
+      getUrlMetadata(articleUrl, (title) => {
+        console.log(title);
       });
     });;
   }
